b5ba4f7afabcd72657f945ecd8465b8b4c1664d1
Aditya9329 <maurya932926@gmail.com>
2023-01-08 21:31:59 -0800

    is_circular and middle_element

--- linkedlist/linkedlist.cpp
+++ linkedlist/linkedlist.cpp
@@ -70,6 +70,59 @@ void delete_at_last(struct node * &head)
 }
 
 // #################################################################################
+
+int count_nodes(struct node * head)
+{
+    int count = 0;
+    while(head != NULL)
+    {
+        head = head->next;
+        count++;
+    }
+    return count;
+}
+
+void middle_element(struct node* head){
+    int list_length = count_nodes(head);
+    int middle = list_length /2;
+    int i = 0;
+    while(i != middle)
+    {
+        head = head->next;
+        i++;
+    }
+    cout<<"middle-element = "<<head->data;
+
+}
+
+// #################################################################################
+
+bool iscircular(struct node * head)
+{
+    if(head == NULL)
+    {
+        return true;
+    }
+    else
+    {
+        struct  node * temp = head->next;
+        while (temp !=NULL && temp!=head)
+        {
+            temp = temp->next;
+        }
+        if(temp == head)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+}
+
+// #################################################################################
 void print_list(struct node* head)
 {
     if(head == NULL)
@@ -92,11 +145,16 @@ void print_list(struct node* head)
 int main()
 {
     struct node *head = NULL;
-    append(head,10);
-    append(head,11);
-    append(head,20);
-    append(head,30);
-    insert_as_first(head,21);
-    delete_at_last(head);
-    print_list(head);
+    append(head,10);// working properly
+    append(head,11);// working properly
+    append(head,20);// working properly
+    append(head,30);// working properly
+    append(head,35);// working properly
+    insert_as_first(head,21);  // working properly
+    // delete_at_last(head); // working properly
+    // print_list(head); // working properly
+    cout<<endl;
+    // middle_element(head); // working properly
+    // cout<<iscircular(head); //working properly
+
 }
